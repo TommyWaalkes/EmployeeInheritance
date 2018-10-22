b65db7876245ad2d66491e892068235d79d7d294
Tommy Waalkes <Tommy@grandcircus.co>
2018-10-22 13:57:17 -0400

    Initial Commit

---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    class Employee : Person
+    {
+        private string id = "ABC567EFG";
+
+        public Employee(string name, string ssn) :base(name, ssn)
+        {
+
+        }
+        public override void PrintName()
+        {
+            Console.WriteLine(name +" Employee Id: "+id);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+
+
+    class TestClass
+    {
+        static void Main()
+        {
+            // Polymorphism in action
+            //Why are we able to add these different types to our list? 
+            //List<Person> people = new List<Person>();
+
+            //Console.WriteLine();
+            //Console.WriteLine("Print from the foreach Loop");
+            //foreach(Person n in people)
+            //{
+            //    //Which version of getInfo() is being used?
+            //    n.GetInfo();
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
